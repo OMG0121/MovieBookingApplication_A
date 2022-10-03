7c7477da48f48c6dc6e5e3f66053f125f4f10206
OMG0121 <106796509+OMG0121@users.noreply.github.com>
2022-10-03 12:45:16 +0530

    Checkpoint 5

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,21 +1,33 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import Header from '../../../src/common/header/Header';
 import './Home.css';
-import ImageListComp from '../../screens/home/ImageListComponent';
+import TopPortion from '../../screens/home/TopPortion';
 import moviesData from '../../common/moviesData';
+import {BottomPortion} from '../home/MovieFilter';
+import Details from '../../screens/details/Details';
+
+
 class Home extends Component{
-    render(){
+   
+    constructor(props){
+        super(props);
+    }
+    render(){   
         return (
             <div>
-                <Header/>
+               <Header/> 
                 <div id="header2">
-                <span >Upcoming Movies</span>
-
+                    <span >Upcoming Movies</span>
                 </div>
-                <ImageListComp moviesData={moviesData}></ImageListComp>
+                <div>
+                        <TopPortion moviesData={moviesData}></TopPortion>
+                </div>
+                <div className='flex-containter'>
+                    <BottomPortion ></BottomPortion>
+                </div>
             </div>
         )
-    }
-}   
+    }      
+}
 
 export default Home;
